5e00b33d52144283446dbd4ef5e767e767d87ba9
Alexis Fromager <alexisfromager@live.fr>
2019-01-21 20:56:51 +0100

    ajout d'un fichier .gitignore dans le back

---
+++ back/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+      "browser": false,
+      "node": true,
+      "jasmine": true
+    },
+    "rules": {
+      "no-console": 0,
+      "comma-dangle": 0,
+      "arrow-parens": 0,
+      "no-restricted-globals": 0
+    }
+};
